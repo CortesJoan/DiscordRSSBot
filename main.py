ac98358c7104699e01a6bf3cb62acfa283c89719
Joan <cortesjoancontact@gmail.com>
2024-04-08 20:54:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+Aquí está el código completo con la última versión:
+
+```python
 import keep_alive
 import discord
 import os
@@ -16,6 +19,7 @@ from firebase_admin import credentials
 from firebase_admin import db
 import json
 from flask import redirect
+from urllib.parse import quote
 
 interval = 10 # change this to the number of seconds between each check
 emote_to_put_at_message_start = "<:Yossixhehe:1109926657613103154>"
@@ -35,23 +39,21 @@ firebase_admin.initialize_app(cred_object, {
 })  # Replace this with your database URL
 
 ref = db.reference('/')
+
+# Crear los nodos si no existen
 if not ref.child("last_message").get():
     ref.child("last_message").set({})
 
 if not ref.child("sent_links").get():
     ref.child("sent_links").set({})
+
 bot_data_ref = ref.child("last_message")
 sent_links_ref = ref.child("sent_links")
-if not sent_links_ref.get():
-    sent_links_ref.set({})
-if not bot_data_ref.get():
-    bot_data_ref.set({})
-if not bot_data_ref.child("last_message").get():
-    bot_data_ref.child("last_message").set("")
-if not bot_data_ref.child("last_link").get():
-    bot_data_ref.child("last_link").set("")
 
-
+@client.event
+async def on_ready():
+    print("bot online" )
+    send_rss.start()
 
 @client.command()
 async def ping(ctx):
@@ -88,8 +90,34 @@ async def add(interaction: discord.Interaction, first_value: int, second_value: 
     """Adds two numbers together."""
     await interaction.response.send_message( f'{first_value} + {second_value} = {first_value + second_value}')
 
-
-
+@tasks.loop(seconds=interval)
+async def send_rss():
+    print("Time to check")
+    global last_message
+    global last_link
+    new_messages = prepare_new_rss()
+    if new_messages:
+        for channel_id in channel_ids:
+            channel = client.get_channel(channel_id)
+            if channel is not None:
+                for new_message in new_messages:
+                    link = new_message["link"]
+                    encoded_link = quote(link, safe='')  # Codificar el enlace
+                    if not sent_links_ref.child(encoded_link).get():  # Verificar si el enlace ya ha sido enviado
+                        print("New content to send")
+                        await channel.send(new_message["message"])
+                        print("Message sent")
+                        sent_links_ref.child(encoded_link).set(True)  # Marcar el enlace como enviado
+                    else:
+                        print("No new content to send")
+            else:
+                print(f"Could not find channel with ID {channel_id}")
+        last_message = new_messages[-1]["message"]
+        last_link = new_messages[-1]["link"]
+        save_last_message(last_message)
+        save_last_link(last_link)
+        print(last_message)
+    await asyncio.sleep(interval)
 
 @client.command()
 async def force_rss_get(ctx, number: int):
@@ -105,7 +133,7 @@ async def force_rss_get(ctx, number: int):
 def prepare_new_rss():
     global last_link
     global last_message
-    new_messages = list()
+    new_messages = []
     try:
         final_url = rss_base_domain + rss_account
         feed = feedparser.parse(final_url)
@@ -157,42 +185,6 @@ def prepare_specific_rss(number: int):
         print("Error while parsing RSS feed: ", e)
     return message
 
-
-@client.event
-async def on_ready():
-    print("bot online" )
-    # Crear el nodo 'sent_links' si no existe
-   
-    send_rss.start() 
-@tasks.loop(seconds=interval)
-async def send_rss():
-    print("Time to check")
-    global last_message
-    global last_link
-    new_messages = prepare_new_rss()
-    if new_messages:
-        for channel_id in channel_ids:
-            channel = client.get_channel(channel_id)
-            if channel is not None:
-                for new_message in new_messages:
-                    link = new_message["link"]
-                    if not sent_links_ref.child(link).get():  # Verificar si el enlace ya ha sido enviado
-                        print("New content to send")
-                        await channel.send(new_message["message"])
-                        print("Message sent")
-                        sent_links_ref.child(link).set(True)  # Marcar el enlace como enviado
-                    else:
-                        print("No new content to send")
-            else:
-                print(f"Could not find channel with ID {channel_id}")
-        last_message = new_messages[-1]["message"]
-        last_link = new_messages[-1]["link"]
-        save_last_message(last_message)
-        save_last_link(last_link)
-        print(last_message)
-    await asyncio.sleep(interval)
-# ... (resto del código sin cambios)
-
 def save_last_message(message):
     global last_link
     bot_data_ref.update({"last_message": message})
@@ -201,15 +193,14 @@ def save_last_link(link):
     bot_data_ref.update({"last_link": link})
 
 def load_last_message():
-    return bot_data_ref.get()["last_message"]
-
+    return bot_data_ref.get("last_message", "")
 
 def load_last_link():
-    return bot_data_ref.get()["last_link"]
-
+    return bot_data_ref.get("last_link", "")
 
 keep_alive()
 last_message = load_last_message()
 last_link = load_last_link()
 print("the saved message" + last_message + "with link: " + last_link)
 client.run(os.environ.get("TOKEN"))
+ 
