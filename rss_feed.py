8bdcf56889fdd2c4c671e3c0498b525c0782f71f
Joan <cortesjoancontact@gmail.com>
2024-04-12 13:12:35 +0200

    Important fic

--- rss_feed.py
+++ rss_feed.py
@@ -10,6 +10,8 @@ class RSSFeed:
     def get_new_messages(self):
         new_messages = []
         try:
+            sent_links_data = self.firebase_service.sent_links_ref.get()
+            sent_links = [link for link in sent_links_data.values()] if sent_links_data else []
             for rss_base_domain in self.rss_base_domains:
                 final_url = rss_base_domain + self.rss_account
                 feed = feedparser.parse(final_url)
@@ -18,11 +20,13 @@ class RSSFeed:
                         link = entry.link
                         base_domain_pattern = re.escape(rss_base_domain)
                         link = re.sub(base_domain_pattern, 'https://fxtwitter.com', link)
-                        message = f"🧸| {entry.title}\n{link}"
-                        message = re.sub(r'<[^>]*>', '', message)
-                        message = re.sub("🧸", self.emote_to_put_at_message_start, message)
-                        message = re.sub("@Hobbyfiguras: ", '', message)
-                        new_messages.append({"message": message, "link": link})
+                        if link not in sent_links:
+                            message = f"🧸| {entry.title}\n{link}"
+                            message = re.sub(r'<[^>]*>', '', message)
+                            message = re.sub("🧸", self.emote_to_put_at_message_start, message)
+                            message = re.sub("@Hobbyfiguras: ", '', message)
+                            new_messages.append({"message": message, "link": link})
+                            self.firebase_service.save_sent_link(link)
         except Exception as e:
             print("Error while parsing RSS feed: ", e)
         return new_messages
