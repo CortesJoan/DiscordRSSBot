c41f5ee77b1432173ebbe50baeebb3da437ecc5b
Joan <cortesjoancontact@gmail.com>
2024-04-12 15:04:09 +0200

    Create test_bot.py

---
+++ test_bot.py
@@ -0,0 +1,19 @@
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+
+bot = commands.Bot(command_prefix='loli-', intents=intents)
+
+@bot.command(name='ping')
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+
+bot.run(os.environ.get('TOKEN'))
